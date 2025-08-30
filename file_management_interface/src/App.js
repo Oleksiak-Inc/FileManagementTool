580a3e474cf4dfed17bb6ae1f93ecc66ce7d37f1
Krzysztof Oleksiak <krzysztof.oleksiak@lionbridge.com>
2025-08-30 15:24:33 +0200

    Creared CSS and APP.js, added a few first lines in App.js

---
+++ file_management_interface/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <tr>
+        <td>ID</td>
+        <td>Title</td>
+        <td>Description</td>
+        <td>PC</td>
+      </tr>
+    </div>
+  );
+}
+
+export default App;
